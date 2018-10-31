a9991e2c3a5575ed80527924dd21551c71f9ecc9
nwmaker <makernw@gmail.com>
2018-10-31 13:05:16 -0600

    add-counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
-function Counter() {
+const Counter2 = () => {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `Counter = ${count}`;
+  })
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
-        Click me
+        Increment
+      </button>
+      <button onClick={() => setCount(0)}>
+        Clear
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
       </button>
     </div>
   )
 }
 
-export default Counter
+export default Counter2
 
